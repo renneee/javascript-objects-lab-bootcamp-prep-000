44a0b71423cef735fc0a6c8fd7b8c4d4632b43da
renneee <renpyang@gmail.com>
2017-08-25 06:52:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,8 +21,8 @@ function destructivelyUpdateObjectWithKeyAndValue (recipes, ingredient, amount) 
 
 function deleteFromObjectByKey (recipes, amount) {
   var newRecipe = Object.assign( {}, amount)
-  
+
   delete newRecipe.amount
-  
+
   return newRecipe
 }
