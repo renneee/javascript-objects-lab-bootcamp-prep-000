4c098a8b6275a591ea9c28d5fc07a417947a31f7
renneee <renpyang@gmail.com>
2017-08-25 06:42:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -8,3 +8,7 @@ function updateObjectWithKeyAndValue (recipes, ingredient, amount) {
 
   return recipes
 }
+
+function updateObjectWithKeyAndValue (recipes, ingredient, amount) {
+  return Object.assign({}, recipes, {[ingredient]:amount})
+}
