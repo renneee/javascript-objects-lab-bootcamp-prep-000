9216a8b9b4924337585d9c4b3c83991e92441c2e
renneee <renpyang@gmail.com>
2017-08-25 06:38:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,9 @@ var recipes = {
   eggs : "3",
   flour : "3 cups"
 };
+
+function updateObjectWithKeyAndValue (recipes, ingredient, amount) {
+  recipes[ingredient] = 'amount'
+  
+  return recipes
+}
